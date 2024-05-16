715fbb64426dc65ef40976dafadafba77d3d4692
tejasree006 <tejasreemuttoju@gmail.com>
2024-05-17 03:41:52 +0530

    Add files via upload

---
+++ TASK 1.py
@@ -0,0 +1,42 @@
+#create a list
+my_list=[1,2,3,4,5]
+print(my_list) 
+#add an element to the list
+my_list.append(6)
+print(my_list)
+#remove an element to the list
+my_list.remove(3)
+print(my_list)
+#modify the element
+my_list[3]=8
+print(my_list)
+
+
+#create an dictionary
+my_dict={'name':'Ruby','age':18,'hobby':'dancing'}
+print(my_dict)
+#adding key value pair
+my_dict['gender']='Female'
+print(my_dict)
+#removing key value pair
+del my_dict['age']
+print(my_dict)
+#modifying an key value pair
+my_dict['name']='Teja'
+print(my_dict)
+
+
+#create a set
+my_set={1,2,3,4,5}
+print(my_set)
+#add an element to the set
+my_set.add(6)
+print(my_set)
+#remove an element to the set
+my_set.remove(2)
+print(my_set)
+#modify the given set
+my_set.update({9})
+print(my_set)
+
+
